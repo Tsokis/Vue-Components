a14592894d835f0e0f668d5fae35758b87d19a5d
Nikos Ntafopoulos <nik.ntf1@gmail.com>
2019-07-19 13:04:59 +0300

    Just a test component

---
+++ src/components/TestComp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <!-- passing html or whatever with slots -->
+        <slot>{{ test }}</slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                test:'some test'
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
